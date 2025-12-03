8d81f1ff82c1124a15431d1cdebc9a215fd11750
Imran <imranchowdhury144@gmail.com>
2025-12-03 15:28:57 +0600

    fix(user): fix serializers fields

--- courses/serializers.py
+++ courses/serializers.py
@@ -7,25 +7,28 @@ class CategorySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Category
-        fields = '__all__'
+        fields = ['id','name','slug','created_at','updated_at','courses']
         read_only_fields =['slug']
 
 
 class CourseSerializer(serializers.ModelSerializer):
     # instructor_detail = UserDetailSerializer(source='instructor', read_only=True)
     lessons = serializers.StringRelatedField(many=True, read_only = True)
+    category = serializers.StringRelatedField()
+    instructor = serializers.StringRelatedField()
 
     class Meta:
         model = Course
-        fields = '__all__'
+        fields = ['id','title','slug','category','instructor','banner','description','duration','is_active','price','lessons','created_at','updated_at']
         read_only_fields = ('instructor',)
 
 
 class LessonSerializer(serializers.ModelSerializer):
     materials = serializers.StringRelatedField(many=True, read_only = True)
+    course = serializers.StringRelatedField()
     class Meta:
         model = Lesson
-        fields = '__all__'
+        fields = ['id','order','title','description','video','course','materials','created_at','updated_at']
 
 
 class MaterialSerializer(serializers.ModelSerializer):
